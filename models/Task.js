6ba964395603f8e9a3dcce0c85964107921a3513
Kunj Bhatia <kunj.bhatia@w3villa.com>
2026-01-06 10:46:02 +0530

    configured db, and created user and task models

---
+++ models/Task.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    completed: { type: Boolean, default: false },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", TaskSchema);
