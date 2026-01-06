aebbdbf7076ac4ae81b603be1a14dddca8536d89
Kunj Bhatia <kunj.bhatia@w3villa.com>
2026-01-06 11:07:35 +0530

    add authentication controller and task management functions

---
+++ controllers/authController.js
@@ -0,0 +1,43 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const User = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: "User registered successfully" });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  res.status(200).json({ token });
+};
---
+++ controllers/taskController.js
@@ -0,0 +1,38 @@
+const Task = require("../models/Task");
+
+exports.createTask = async (req, res) => {
+  const task = await Task.create({
+    ...req.body,
+    user: req.user.id,
+  });
+  res.status(201).json(task);
+};
+
+exports.getTasks = async (req, res) => {
+  const task = await Task.findOneAndUpdate(
+    { _id: req.params.id, user: req.user.id },
+    req.body,
+    { new: true }
+  );
+  if (!task) {
+    return res.status(404).json({ message: "Task not found" });
+  }
+
+  res.status(200).json(task);
+};
+
+exports.updateTask = async (req, res) => {
+  const tasks = await Task.find({ user: req.user.id });
+  res.status(200).json(tasks);
+};
+
+exports.deleteTask = async (req, res) => {
+  const task = await Task.findOneAndDelete({
+    _id: req.params.id,
+    user: req.user.id,
+  });
+  if (!task) {
+    return res.status(404).json({ message: "Task not found" });
+  }
+  res.status(200).json({ message: "Task deleted successfully" });
+};
